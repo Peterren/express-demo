e30e73be8d8d8e075bf4c36ea8c51650736b2244
Yincheng Ren <yr5ka@virginia.edu>
2020-06-18 15:57:28 -0400

    CRUD

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
 
+app.use(express.json());
 const courses = [
     {id : 1, name: 'course1'},
     {id : 2, name: 'course2'},
@@ -15,11 +17,53 @@ app.get('/api/courses',(req, res)=>{
 });
 
 app.get('/api/courses/:id',(req, res)=>{
+
     const course = courses.find(c =>c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('Course not find');
-    console.log(course);
+    if (!course) return res.status(404).send('Course not find');
     res.send(course);
 });
+
+app.post('/api/courses/', (req,res) =>{
+    const {error} = validateCourse(req.body);
+    if (error) return res.status(400).send(result.error.details[0].message);
+    const course = {
+        id: courses.length+1,
+        name: req.body.name    
+    };
+    courses.push(courses);
+    res.send(course);
+})
+
+
+app.put('/api/courses/:id',(req,res)=>{
+    const course = courses.find(c =>c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('Course not find');
+
+    const {error} = validateCourse(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    course.name = req.body.name;
+    res.send(course);
+})
+
+function validateCourse(course){
+    const schema = {
+        name: Joi.string().min(3).required()
+    }
+    return Joi.validate(course,schema);
+}
+
+app.delete('/api/courses/:id', (req,res)=>{
+    const course = courses.find(c =>c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('Course not find');
+
+    const index = courses.indexOf(course);
+    courses.splice(index,1);
+
+    res.send(course);
+})
+
+
 
 //PORT
 const port = process.env.PORT || 3000
